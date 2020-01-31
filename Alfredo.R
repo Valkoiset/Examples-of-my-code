3f8ed210923df40e9b8b7dfc6bd0186c87c61b63
Oleksandr Romanchenko <36264408+Valkoiset@users.noreply.github.com>
2020-01-31 14:19:31 +0100

    Add files via upload

---
+++ Alfredo.R
@@ -0,0 +1,37 @@
+
+
+library(dplyr)
+library(stringr)
+library(splitstackshape)
+
+rm(list = ls())
+setwd("C:/Users/oleksandr-romanchenk/Desktop/Alfredo")
+mydata <- read.csv("Country-City-List.csv")
+
+mydata <- mydata %>% mutate_at(.vars = vars(s_country_name, s_array_city), .funs = as.character) %>% 
+  mutate(s_array_city = gsub("\\[|\\]", "", s_array_city)) 
+
+cities <- strsplit(mydata$s_array_city, split = ";")
+unique(cities[[233]])[[1]]
+
+for (row in seq_along(cities)) {
+  if (length(cities[[row]]) > 1) {
+    for (val in 2:length(cities[[row]])) {
+      # it's important to overwrite dataset here, otherwise if it is written as
+      # newdata <- mydata %>% rbind(mydata[row, ]) 
+      # it creates new dataset with 1 additional row and in the end gives you dataset which each is
+      # only 1 row more than initial one (254 --> 255 instead of 254 --> 462)
+      mydata <- mydata %>% rbind(mydata[row, ]) 
+      mydata[nrow(mydata), ] <- mydata[nrow(mydata), ] %>% mutate(s_array_city = cities[[row]][[val]])
+    }
+  }
+}
+
+mydata <- mydata %>% arrange(s_country_name) %>% 
+  mutate(s_array_city = gsub(";.*", "", s_array_city))
+
+mydata %>% select(s_country_name, s_array_city) %>% View()
+
+write.csv(mydata, "mydata.csv")
+
+
